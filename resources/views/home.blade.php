e80d50f798f205a6bc3f76f83fbee25ec0cdcdf7
anto991nio <antonio.91.cardone@gmail.com>
2021-06-23 10:23:08 +0200

    Corregge esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="{{asset('css\app.css')}}">
+    <link rel="stylesheet" href="{{asset('css\app.css')}}">
     <title>Document</title>
 </head>
 <body>
@@ -15,11 +15,11 @@
         @foreach($moviesList as $movie)
 
         <div class="card">
-            <p class=""><strong> Titolo: {{ $movie['title'] }}</strong></p>
-            <p class=""><strong> Titolo originale: {{ $movie['original_title'] }}</strong></p>
-            <p class=""><strong> Nazionalità: {{ $movie['nationality'] }}</strong></p>
-            <p class=""><strong> Data uscita:{{ $movie['date'] }}</strong></p>
-            <p class=""><strong> Voto: {{ $movie['vote'] }}</strong></p>
+            <p class=""><strong> Titolo: {{ $movie->title }}</strong></p>
+            <p class=""><strong> Titolo originale: {{ $movie->original_title }}</strong></p>
+            <p class=""><strong> Nazionalità: {{ $movie->nationality }}</strong></p>
+            <p class=""><strong> Data uscita:{{ $movie->date }}</strong></p>
+            <p class=""><strong> Voto: {{ $movie->vote }}</strong></p>
         </div>
 
 
